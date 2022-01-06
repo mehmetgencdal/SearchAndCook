5ba22c6745427cc00629a334fc351478ca35719e
Mehmet Gençdal <181805063@stu.adu.edu.tr>
2022-01-06 23:23:17 +0300

    search bar done!

--- lib/Homepage/database.dart
+++ lib/Homepage/database.dart
@@ -27,3 +27,37 @@ query() async {
 
   return result;
 }
+
+query2() async {
+  // get a reference to the database
+  Database db = await database();
+
+  // get all rows
+  List<Map> result = await db.query("ingredients");
+  List ingName = [];
+  var i = result.length;
+  var j = 0;
+  while (j < i) {
+    ingName.add(result[j]["ingName"]);
+    j++;
+  }
+
+  return ingName;
+}
+
+query3() async {
+  // get a reference to the database
+  Database db = await database();
+
+  // get all rows
+  List<Map> result = await db.query("ingredients");
+  var ingInf = {};
+  var i = result.length;
+  var j = 0;
+  while (j < i) {
+    ingInf[result[j]["ingName"]] = result[j]["ingIcon"];
+    j++;
+  }
+
+  return ingInf;
+}
